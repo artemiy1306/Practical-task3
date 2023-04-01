4a4a92172d6c10ec0560e1804367ab5a66224a0b
artemiy <karelskiy@mail.ru>
2023-04-01 19:45:30 +0300

    add zad23

---
+++ zad23/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+for (int i = 1; i <= number;i++) 
+{
+double cube = Math.Pow(i,3);
+Console.WriteLine($"{cube}");
+}
+
